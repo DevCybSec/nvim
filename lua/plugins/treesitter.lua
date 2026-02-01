928d450b9b941da1f867ce1bacf2299b395c1744
DevCybSec <edgarmacias@devcybsec.com>
2026-01-31 18:45:22 -0600

    fix to treesitter made

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,18 @@ return {
 	"nvim-treesitter/nvim-treesitter",
   	build = ":TSUpdate",
     	config = function()
-	        require("nvim-treesitter.configs").setup({
-			ensure_installed = { "go", "typescript", "javascript", "lua", "json", "html" },
+		
+		local configs = require("nvim-treesitter-config")
+
+      		configs.setup({
+			ensure_installed = {
+			 "go","gosum","gomod","gowork",
+			 "typescript", "javascript","tsx",
+			 "lua","vim","vimdoc","jquery","markdown","markdown_inline",
+			 "json", "html" },
+			sync_install=false,
 			highlight = { enable = true },
 			indent = { enable = true },
-		})
-	       end,
+	})
+        end,
 }
